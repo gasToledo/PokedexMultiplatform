6ff58977e7940025601ed44f96a9ab6231af1dba
gasToledo <mgaston648@gmail.com>
2023-09-10 17:28:50 -0300

    FirstCommit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PokedexMultiPlatform"
+include(":androidApp")
+include(":shared")
